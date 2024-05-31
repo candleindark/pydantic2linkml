bcfff9a8885f1ba61f0521651894451e6e77bf5f
Isaac To <candleindark@gmail.com>
2024-05-31 11:36:20 -0700

    Reformat generate_linkml_from_aind.py with Black

--- generate_linkml_from_aind.py
+++ generate_linkml_from_aind.py
@@ -12,20 +12,17 @@ from linkml_runtime.utils.schemaview import PermissibleValue, PermissibleValueTe
 from linkml_runtime.utils.schema_builder import SchemaBuilder
 from linkml_runtime.dumpers import yaml_dumper
 
-
 app = typer.Typer()
 
-
-# BICAN already has linkml here: https://github.com/brain-bican/models/tree/main/linkml-schema
-# Biolink also has linkml: https://github.com/biolink/biolink-model/blob/master/src/biolink_model/schema/biolink_model.yaml
+# BICAN already has linkml here:
+#   https://github.com/brain-bican/models/tree/main/linkml-schema
+# Biolink also has linkml:
+#   https://github.com/biolink/biolink-model/blob/master/src/biolink_model/schema/biolink_model.yaml
 # openminds is JSON: https://github.com/openMetadataInitiative/openMINDS_core/tree/v4
 # ATOM: https://bioportal.bioontology.org/ontologies/ATOM
 # ATOM: https://github.com/SciCrunch/NIF-Ontology/blob/atlas/ttl/atom.ttl
 # ATOM: https://www.nature.com/articles/s41597-023-02389-4
-KNOWN_MODELS = {
-    'dandi': 'dandischema.models',
-    'aind': 'aind_data_schema.models'
-}
+KNOWN_MODELS = {"dandi": "dandischema.models", "aind": "aind_data_schema.models"}
 
 
 def get_all_modules(imported_modules: list, root_module_name: str):
@@ -33,36 +30,53 @@ def get_all_modules(imported_modules: list, root_module_name: str):
         module = importlib.import_module(root_module_name)
         imported_modules.append(module)
         for submodule_filename in module.__loader__.get_resource_reader().contents():
-            if submodule_filename.endswith('.py') and not submodule_filename.startswith('__'):
-                get_all_modules(imported_modules, f'{root_module_name}.{submodule_filename[:-len(".py")]}')
+            if submodule_filename.endswith(".py") and not submodule_filename.startswith(
+                "__"
+            ):
+                get_all_modules(
+                    imported_modules,
+                    f'{root_module_name}.{submodule_filename[:-len(".py")]}',
+                )
     except ModuleNotFoundError:
         return imported_modules
     return imported_modules
 
 
 def populate_enum(sb: SchemaBuilder, enum_name: str, enum_object: enum.Enum):
-    """Populate a LinkML SchemaBuilder instance with a new enum derived from a pydantic Enum object."""
+    """
+    Populate a LinkML SchemaBuilder instance with a new enum derived from
+    a pydantic Enum object.
+    """
     try:
         sb.add_enum(
             EnumDefinition(
                 name=enum_name,
                 permissible_values=dict(
-                    (attribute, getattr(enum_object, attribute).value) for attribute in dir(enum_object) if
-                    not attribute.startswith('__') and isinstance(getattr(enum_object, attribute), enum.Enum))
+                    (attribute, getattr(enum_object, attribute).value)
+                    for attribute in dir(enum_object)
+                    if not attribute.startswith("__")
+                    and isinstance(getattr(enum_object, attribute), enum.Enum)
+                ),
             )
         )
     except ValueError as e:
-        if not 'already exists' in str(e):
+        if not "already exists" in str(e):
             raise
 
 
-def populate_basemodel(sb: SchemaBuilder, basemodel_name: str, basemodel_object: pydantic.BaseModel):
+def populate_basemodel(
+    sb: SchemaBuilder, basemodel_name: str, basemodel_object: pydantic.BaseModel
+):
     sb.add_class(
         basemodel_name,
         slots=basemodel_object.__annotations__,
         is_a=basemodel_object.__mro__[1].__name__,
-        class_uri=f'schema:{basemodel_name}',
-        description=basemodel_object.__doc__.strip() if basemodel_object.__doc__ else "No description"
+        class_uri=f"schema:{basemodel_name}",
+        description=(
+            basemodel_object.__doc__.strip()
+            if basemodel_object.__doc__
+            else "No description"
+        ),
     )
 
 
@@ -76,15 +90,18 @@ def populate_schema_builder_from_module(sb: SchemaBuilder, module: str):
 
 
 @app.command()
-def main(root_module_name: str = KNOWN_MODELS['aind'], output_file: Path = Path('generated_linkml_models/aind.yml')):
-    org = 'dandi'
+def main(
+    root_module_name: str = KNOWN_MODELS["aind"],
+    output_file: Path = Path("generated_linkml_models/aind.yml"),
+):
+    org = "dandi"
     sb = SchemaBuilder()
     populate_schema_builder_from_module(sb, module=root_module_name)
     yml = yaml_dumper.dumps(sb.schema)
-    with output_file.open('w') as f:
+    with output_file.open("w") as f:
         f.write(yml)
-    print('Success!')
+    print("Success!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     typer.run(main)
