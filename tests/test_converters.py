62902be131aaeb31169147d77ca2c3b279e28782
Isaac To <candleindark@gmail.com>
2024-07-24 15:24:12 -0700

    Mark `test_smoke()` as `xfail`
    The translation of all Pydantic filed types to be completed

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -1,5 +1,11 @@
+import pytest
+
 from generate_linkml_from_pydantic import main
 
+
+@pytest.mark.xfail(
+    reason="Awaiting the translation of Pydantic filed types to complete"
+)
 def test_smoke():
     """Check that I can run this at all"""
     main(["dandischema.models"])
