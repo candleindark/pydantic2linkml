c6303ee71d4906cad1767134ff4698bf35405146
Isaac To <candleindark@gmail.com>
2024-07-24 12:38:09 -0700

    Update `test_smoke_cli()`
    The app object is no longer available in `generate_linkml_from_pydantic`

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,15 @@
 from typer.testing import CliRunner
+import typer
 
-from generate_linkml_from_pydantic import app
+from generate_linkml_from_pydantic import main
 
 runner = CliRunner()
 
 
 def test_smoke_cli():
+    # Mimic the app creation in generate_linkml_from_pydantic
+    app = typer.Typer()
+    app.command()(main)
+
     result = runner.invoke(app)
     assert result.exit_code == 0
