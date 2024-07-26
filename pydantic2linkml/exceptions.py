ec65faf264f1fbea3213803530228ed4c2e56a7e
Isaac To <candleindark@gmail.com>
2024-07-26 11:31:30 -0700

    Define `TranslationNotImplementedError`

--- pydantic2linkml/exceptions.py
+++ pydantic2linkml/exceptions.py
@@ -13,3 +13,16 @@ class UserError(Exception):
     """
 
     pass
+
+
+class TranslationNotImplementedError(NotImplementedError):
+    """
+    Raise when the translation of a Pydantic core schema to LinkMK is not implemented
+
+    Note: This is used to mark the translation methods of Pydantic core schemas that
+      are deemed to be not necessary for use of this translation tool in general or
+      against the targeted models expressed in Pydantic. File an issue if this error is
+      encountered.
+    """
+
+    pass
