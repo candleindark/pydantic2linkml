2700e87fa83c3c3126c1b793c12eb6ad4d11420c
Isaac To <candleindark@gmail.com>
2024-07-29 10:56:28 -0700

    Remove unneeded `xfail` pytest marks
    The translation of Pydantic field types are not complete,
    at least for Dandischema

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,9 +7,6 @@ from generate_linkml_from_pydantic import main
 runner = CliRunner()
 
 
-@pytest.mark.xfail(
-    reason="Awaiting the translation of Pydantic filed types to complete"
-)
 def test_smoke_cli():
     # Mimic the app creation in generate_linkml_from_pydantic
     app = typer.Typer()
--- tests/test_converters.py
+++ tests/test_converters.py
@@ -3,9 +3,6 @@ import pytest
 from generate_linkml_from_pydantic import main
 
 
-@pytest.mark.xfail(
-    reason="Awaiting the translation of Pydantic filed types to complete"
-)
 def test_smoke():
     """Check that I can run this at all"""
     main(["dandischema.models"])
