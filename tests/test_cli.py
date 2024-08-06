e646ccf79297a86cfc5bc58a3644e62de7b93014
Isaac To <isaac.chun.to@gmail.com>
2024-08-05 23:53:08 -0700

    Move and rename test from test_converters.py to test_cli.py
    The test is CLI related test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,6 @@
 from typer.testing import CliRunner
 
-from pydantic2linkml.cli import app
+from pydantic2linkml.cli import app, main
 
 runner = CliRunner()
 
@@ -8,3 +8,8 @@ runner = CliRunner()
 def test_smoke_cli():
     result = runner.invoke(app, ["dandischema.models"])
     assert result.exit_code == 0
+
+
+def test_cli_command_func():
+    """Test calling the CLI command function directly"""
+    main(["dandischema.models"])
--- tests/test_converters.py
+++
@@ -1,6 +0,0 @@
-from pydantic2linkml.cli import main
-
-
-def test_smoke():
-    """Check that I can run this at all"""
-    main(["dandischema.models"])
