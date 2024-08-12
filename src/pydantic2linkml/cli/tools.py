d0f3de960f0b13d88a470b247c0230fbb02736c9
Isaac To <isaac.chun.to@gmail.com>
2024-08-11 19:50:13 -0700

    Remove the use of `_generate_next_value_()` in `Enum` subclass
    Its use is a bit inconsistent across Python version.
    In older Python, 3.9 for example, declaring the func as static
    will result in an error.

--- src/pydantic2linkml/cli/tools.py
+++ src/pydantic2linkml/cli/tools.py
@@ -1,15 +1,10 @@
-from enum import auto
-
 from ..tools import StrEnum
 
 
 class LogLevel(StrEnum):
-    @staticmethod
-    def _generate_next_value_(name, _start, _count, _last_values):
-        return name
 
-    DEBUG = auto()
-    INFO = auto()
-    WARNING = auto()
-    ERROR = auto()
-    CRITICAL = auto()
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
